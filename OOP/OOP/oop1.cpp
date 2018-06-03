3de23a5c5fa399a79cca41bed11013826813201e
dianay07 <dianay07@naver.com>
2018-06-04 00:52:46 +0900

    new oop

--- OOP/OOP/oop1.cpp
+++ OOP/OOP/oop1.cpp
@@ -1,116 +1,54 @@
-#include "oop1.h"
-oop1::oop1()
+#include "OOP1.h"
+OOP1::OOP1()
 {
 }
-oop1::~oop1()
+OOP1::~OOP1()
 {
 }
 
-void oop1::showMenu()
+void OOP1::showMenu()
 {
-	cout << "----------∏ﬁ¥∫-------------" << endl;
-	cout << "1. ∞Ë¡¬∞≥º≥ " << endl;
-	cout << "2. ¿‘±› " << endl;
-	cout << "3. √‚±› " << endl;
+	cout << "----MENU----" << endl;
+	cout << "1. ∞Ë¡¬ ∞≥º≥" << endl;
+	cout << "2. ¿‘ ±›" << endl;
+	cout << "3. √‚ ±›" << endl;
 	cout << "4. ∞Ë¡¬¡§∫∏ ¿¸√º √‚∑¬" << endl;
 	cout << "5. «¡∑Œ±◊∑• ¡æ∑·" << endl;
 }
 
-// ∞Ë¡¬ª˝º∫
-void oop1::makeAccount()
+void OOP1::makeAccount()
 {
-	int id;
-	char name[maxNameSpace];
-	int money;
-
-	cout << "[∞Ë¡¬∞≥º≥]" << endl;
-	cout << "ID∏¶ ¿‘∑¬«ÿ¡÷ººø‰ : " << endl; cin >> id;
-	cout << "¿Ã∏ß¿ª ¿‘∑¬«ÿ¡÷ººø‰ : " << endl; cin >> name;
-	cout << "¿‘±›æ◊¿ª ¿‘∑¬«ÿ¡÷ººø‰ : " << endl; cin >> money;
-
-	//πËø≠ø° ∞Ë¡¬¡§∫∏ ¿˙¿Â
-	arr[havingAccount].ID = id;
-	strcpy_s(arr[havingAccount].clientName, name);	// ¿Ø¿«;
-	arr[havingAccount].stockMoney = money;
-
-	//πËø≠¿ª ¿ß∑Œ
-	havingAccount++;
+	cout << "øπ±›¡÷∏Ì : ";
+	cin >> bankUser[accNum].userName;
+	cout << "ID : ";
+	cin >> bankUser[accNum].ID;
+	cout << "√ ±‚ ¿‘±›æ◊ :";
+	cin >> bankUser[accNum].money;
+	accNum++;
 }
 
-// ¿‘±›
-void oop1::depositMoney()
+void OOP1::deposit()
 {
-	int id;
-	int money;
-
-	cout << "∞Ë¡¬ ID∏¶ ¿‘∑¬«œººø‰." << endl;
+	int id, money;
+	cout << "ID∏¶ ¿‘∑¬ :";
 	cin >> id;
 
-	for (int i = 0; i < havingAccount; i++)
+	for (int i = 0; i < accNum; i++)
 	{
-		// ∞Ë¡¬ ID √º≈©
-		if (arr[i].ID == id)
+		if (id == bankUser[accNum].ID)
 		{
-			// ID¿« ¡÷¿Œ¿ª ∫∏ø©¡ÿ¥Ÿ
-			cout << arr[i].clientName << endl;
-			cout << arr[i].stockMoney << endl;
-
-			// ¿‘±› ¿˝¬˜ ¡¯«‡
-			cout << "¿‘±› «“ ±›æ◊¿ª ¿‘∑¬«ÿ¡÷ººø‰ : "; cin >> money;
-			arr[i].stockMoney += money;
-			cout << "¿‘±›øœ∑·" << endl;
-
-			// ∞ËªÍ »ƒ ¡§ªÍ
-			cout << arr[i].stockMoney << endl;
+			cout << "¿‘±›«“ ±›æ◊ :";
+			cin >> money;
+			money += bankUser[accNum].money;
+			cout << "¿‘±› øœ∑·" << endl;
+		}
+		else
+		{
+			cout << "∞Ë¡¬∞° ¡∏¿Á«œ¡ˆ æ ¥¬¥Ÿ" << endl;
 		}
 	}
-	
 }
 
-// ≠y±›
-void oop1::takeMoney()
+void OOP1::withdraw()
 {
-	int id;
-	int money;
-
-	cout << "∞Ë¡¬ ID∏¶ ¿‘∑¬«œººø‰." << endl;
-	cin >> id;
-
-	for (int i = 0; i < havingAccount; i++)
-	{
-		// ∞Ë¡¬ √º≈©
-		if (arr[i].ID == id)
-		{
-			// ID¿« ¡÷¿Œ¿ª ∫∏ø©¡ÿ¥Ÿ
-			cout << arr[i].clientName << endl;
-			cout << arr[i].stockMoney << endl;
-
-			cout << "√‚±› «“ ±›æ◊¿ª ¿‘∑¬«ÿ¡÷ººø‰ : "; cin >> money;
-
-			if (money > arr[i].stockMoney)
-			{
-				cout << "¿‹æ◊∫∏¥Ÿ √‚±›æ◊¿Ã ≈Æ¥œ¥Ÿ. ¥ŸΩ√ ¿‘∑¬«ÿ¡÷ººø‰" << endl;
-				cin >> money;
-			}
-			else
-			{
-				cout << "√‚±› µ«æ˙Ω¿¥œ¥Ÿ." << endl;
-				cout << arr[i].stockMoney << endl;
-			}
-		}
-	}
-
-}
-
-void oop1::showAllClientInfo()
-{
-	for (int i = 0; i < havingAccount; i++)
-	{
-		if (!arr[i].ID == NULL)
-		{
-			cout << arr[i].ID << endl;
-			cout << arr[i].clientName << endl;
-			cout << arr[i].stockMoney << endl;
-		}
-	}
 }
--- OOP/OOP/oop1.h
+++ OOP/OOP/oop1.h
@@ -1,40 +1,35 @@
-// C스타일로 작성하라는데 잘모르겟고 그냥 했다.
 #pragma once
-#include <string>
 #include <iostream>
 using namespace std;
 
-//메뉴 상황 
-enum state
-{
-	MAKE = 1, DEPOSIT = 2, WITHDRAW = 3, SHOW = 4, MENU = 6 , END = 5
-};
+// --------MENU-------
+// 1. 계좌개설
+// 2. 입금
+// 3. 출금
+// 4. 계좌정보 전체 출력
+// 5. 프로그램 종료
 
-//이름 최대
-const int maxNameSpace = 100;
-
-//계좌 구성
-typedef struct account
+class OOP1
 {
-	int ID;					// 고객 아이디
-	int stockMoney;			// 잔액
-	char clientName[maxNameSpace];		// 고객 이름
-};
+	enum stage {MENU = 0, MAKE, DEPOSIT, WITHDRAW, SHOW};
 
-class oop1
-{
-protected:
-	account arr[100];
-	int havingAccount = 0;
+	typedef struct _account
+	{
+		int ID;
+		char userName[20];
+		int money;
+	}Account;
+
+private:
+	Account bankUser[100];
+	int accNum = 0;
 
 public:
-	oop1();
-	~oop1();
-	
-	void showMenu();
-	void makeAccount();			// 계좌생성
-	void depositMoney();		// 입금
-	void takeMoney();			// 출금
-	void showAllClientInfo();	// 전체고객 조회
+	OOP1();
+	~OOP1();
+
+	void makeAccount();		// 계좌 개설
+	void deposit();			// 입금
+	void withdraw();		// 출금
 };
 
